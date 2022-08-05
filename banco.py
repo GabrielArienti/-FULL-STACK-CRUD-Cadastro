2fdd3bc258538bb90e1fa273a6357b5fad198589
Gabriel Arienti Barbieri <gabriel17out@hotmail.com>
2022-08-05 16:56:04 -0300

    Delete banco.py

--- banco.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as lite
-
-con = lite.connect('dados.db')
-
-# with con:
-#cur = con.cursor()
-#cur.execute("CREATE TABLE formulario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, telefone TEXT, dia_em DATE, estado TEXT, comentario TEXT)")
